0969c016950872a55f2f81ca7e5742a2aa64f62c
Decision <787362202@qq.com>
2021-10-02 23:21:27 +0800

    add: core class

--- TodoListCSharp/views/AppearanceSetting.xaml.cs
+++ TodoListCSharp/views/AppearanceSetting.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -31,8 +32,8 @@ namespace TodoListCSharp.views {
             }
         }
 
-        private void Slider_MouseMove(object sender, MouseEventArgs e) {
-            return ;
+        private void AppearanceSetting_onDragOver(object sender, DragCompletedEventArgs e) {
+            System.Console.WriteLine(1);
         }
     }
 }
