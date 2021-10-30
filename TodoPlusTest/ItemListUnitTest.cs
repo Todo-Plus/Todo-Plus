b23b4cb273ca3ebe806f891610d811cb2aebc728
Decision <787362202@qq.com>
2021-10-30 22:13:15 +0800

    fixed: unit test & sync icon

--- TodoPlusTest/ItemListUnitTest.cs
+++ TodoPlusTest/ItemListUnitTest.cs
@@ -9,16 +9,24 @@ namespace TodoPlusTest {
         public void AppendAndDeleteItemTest() {
             TodoItem todoItem1 = new TodoItem("test001", "test001");
             TodoItem todoItem2 = new TodoItem("test002", "test002");
+            TodoItem todoItem3 = new TodoItem("test003", "test003");
+            todoItem1.SetIndex(0);
+            todoItem2.SetIndex(1);
+            todoItem3.SetIndex(2);
 
             ItemList list = new ItemList();
+            list.DeleteItem(0);
             list.AppendItem(todoItem1);
             list.AppendItem(todoItem2);
+            list.AppendItem(todoItem3);
 
             System.Collections.Generic.List<TodoItem> todoItems = list.GetItemList();
-            Assert.AreEqual(todoItems.Count, 2);
+            Assert.AreEqual(todoItems.Count, 3);
             Assert.AreEqual<string>(todoItems[0].Title, todoItem1.Title);
             Assert.AreEqual<string>(todoItems[1].Title, todoItem2.Title);
+            Assert.AreEqual<string>(todoItems[2].Title, todoItem3.Title);
 
+            list.DeleteItem(2);
             list.DeleteItem(0);
             todoItems = list.GetItemList();
             Assert.AreEqual<string>(todoItems[0].Title, todoItem2.Title); ;
